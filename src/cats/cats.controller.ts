ad15618be9672482001d70955425498d1b316455
Bekhzod Akhrorov <bekhzod.akhrorov@Admins-MacBook-Pro-2.local>
2020-11-10 16:00:06 +0200

    Post by extracting body of the request

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Req, Post } from '@nestjs/common';
+import { Request } from 'express';
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(@Req() req: Request): string {
+    const body = JSON.stringify(req.body);
+    console.log(body);
+    return `This is in req body ${body}`;
+  }
+
   @Get()
-  sendArspons(): string {
-    return 'This is cats route';
+  sendArspons(@Req() req: Request): string {
+    return `This request has ben arrived ${req.body[0]}`;
   }
 }
