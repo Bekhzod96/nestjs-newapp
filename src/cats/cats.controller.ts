690a63265e6ec460f99bfe9917d971249228afce
Bekhzod Akhrorov <bekhzod.akhrorov@Admins-MacBook-Pro-2.local>
2020-11-10 16:21:00 +0200

    Get access params and sub routing

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,16 +1,43 @@
-import { Controller, Get, Req, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  Post,
+  HttpCode,
+  Header,
+  Redirect,
+  Query,
+  Param,
+} from '@nestjs/common';
 import { Request } from 'express';
 @Controller('cats')
 export class CatsController {
   @Post()
+  @HttpCode(204)
+  @Header('Cache-Control', 'none')
   create(@Req() req: Request): string {
     const body = JSON.stringify(req.body);
     console.log(body);
     return `This is in req body ${body}`;
   }
 
-  @Get()
-  sendArspons(@Req() req: Request): string {
-    return `This request has ben arrived ${req.body[0]}`;
+  // @Get()
+  // sendArspons(@Req() req: Request): string {
+  //   return `This request has ben arrived ${req.body[0]}`;
+  // }
+
+  // --------- Redirect -------
+  // @Get('docs')
+  // @Redirect('https://docs.nestjs.com', 302)
+  // getDocs(@Query('version') version) {
+  //   if (version && version === '5') {
+  //     return { url: 'https://docs.nestjs.com/v5/' };
+  //   }
+  // }
+
+  @Get('doc/:id')
+  findOne(@Param('id') id): string {
+    console.log(id);
+    return `This action returns a #${id} cat`;
   }
 }
